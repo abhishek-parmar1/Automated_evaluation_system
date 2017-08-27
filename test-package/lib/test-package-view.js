9a9884f32f9e5862c71d55be9b205f3c6eb6c938
Abhishek <parmarabhishek.53@gmail.com>
2017-08-28 03:19:59 +0530

    dynamic data transfere between result view and api call is established

--- test-package/lib/test-package-view.js
+++ test-package/lib/test-package-view.js
@@ -6,12 +6,12 @@ export default class TestPackageView {
     // Create root element
     this.element = document.createElement('div');
     this.element.classList.add('test-package');
-
     // Create message element
     const message = document.createElement('div');
-    message.textContent = 'The TestPackage package is Alive! It\'s ALIVE!';
+    message.textContent = "good work buddy, view created";
     message.classList.add('message');
     this.element.appendChild(message);
+
   }
 
   // Returns an object that can be retrieved when package is activated
@@ -20,10 +20,39 @@ export default class TestPackageView {
   // Tear down any state and detach
   destroy() {
     this.element.remove();
+    //this.subscriptions.dispose();
   }
 
-  getElement() {
-    return this.element;
+  getTitle() {
+    // Used by Atom for tab text
+    return 'Test Package Result';
   }
 
+  getURI() {
+    // Used by Atom to identify the view when toggling.
+    return 'atom://test-package';
+  }
+
+  getDefaultLocation() {
+    // This location will be used if the user hasn't overridden it by dragging the item elsewhere.
+    // Valid values are "left", "right", "bottom", and "center" (the default).
+    return 'right';
+  }
+
+  getAllowedLocations() {
+    // The locations into which the item can be moved.
+    return ['left', 'right', 'bottom'];
+  }
+
+  serialize() {
+    return {
+      // This is used to look up the deserializer function. It can be any string, but it needs to be
+      // unique across all packages!
+      deserializer: 'test-package/TestPackageView'
+    };
+  }
+
+  renderAnswer(objective){
+    console.log(objective);
+  }
 }
--- test-package/lib/test-package.js
+++ test-package/lib/test-package.js
@@ -1,67 +1,111 @@
 'use babel';
 
 import TestPackageView from './test-package-view';
-import { CompositeDisposable } from 'atom';
+import { CompositeDisposable, Disposable } from 'atom';
 import request from 'request'
 
 export default {
 
-  testPackageView: null,
-  modalPanel: null,
   subscriptions: null,
 
   activate(state) {
-    this.testPackageView = new TestPackageView(state.testPackageViewState);
-    this.modalPanel = atom.workspace.addModalPanel({
-      item: this.testPackageView.getElement(),
-      visible: false
-    });
-
     // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-    this.subscriptions = new CompositeDisposable();
+    this.subscriptions = new CompositeDisposable(
+      // Add an opener for our view.
+      atom.workspace.addOpener(uri => {
+        if (uri === 'atom://test-package-result') {
+          return new TestPackageView();
+        }
+      }),
 
-    // Register command that toggles this view
-    this.subscriptions.add(atom.commands.add('atom-workspace', {
-      'test-package:fetch': () => this.fetch()
-    }));
+      // Register command that toggles this view
+      atom.commands.add('atom-workspace', {
+        'test-package:fetch': () => this.fetch()
+      }),
+
+      // Destroy any TestPackegeViews when the package is deactivated.
+      new Disposable(() => {
+        atom.workspace.getPaneItems().forEach(item => {
+          if (item instanceof TestPackageView) {
+            item.destroy();
+          }
+        });
+      })
+
+    );
   },
 
   deactivate() {
-    this.modalPanel.destroy();
     this.subscriptions.dispose();
-    this.testPackageView.destroy();
   },
 
-  serialize() {
-    return {
-      testPackageViewState: this.testPackageView.serialize()
-    };
+  deserializeTestPackageView(serialized) {
+    return new TestPackageView();
   },
 
+// function to get the objectives using the api
   fetch() {
+    console.log("call made successfully");
     let editor
+    //  to check get the reference of active editor in atom
     if (editor = atom.workspace.getActiveTextEditor()) {
-      let selection = editor.getSelectedText();
-      this.download(selection).then( function(objective) {
-            editor.insertText(objective);
-          }).catch( function(error) {
-            atom.notifications.addWarning(error.reason);
-          });
+    // static url to call
+    let selection = "http://acadstaging.com/py/api/student/projects/objectives?projectStudentId=1040";
+    // call to download function to get the objectives from api
+    this.download(selection).then( objective => {
+          // call the method to display the result
+          this.showResult("objective to complete the view");
+        }).catch( error => {
+          this.showResult("objective to complete the view");
+          // if error in response display the error message on the modal
+          atom.notifications.addWarning(error.reason);
+        });
     }
   },
 
-    download(url) {
-      return new Promise( function(resolve, reject) {
-        request(url, function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            resolve(body);
-          } else {
-            reject({
-              reason: 'Unable to download page'
-            });
-          }
-        });
+  // function to request the api
+  download(url) {
+    // a request variable which uses request
+    var request = require('request');
+    // request structure
+    var options = {
+      url: url,
+      headers: {
+        'sessionid' : 'l69ps2o0a47knaqtmj5ajlcr6m'
+      }
+    };
+    // return the value obtained after the api request using promises
+    return new Promise( ( resolve, reject) => {
+      // request to api
+      request( options, ( error, response, body) => {
+        if(!error && response.statusCode == 200) {
+          // return response
+          resolve(body);
+        }
+        else {
+          // resturn error object for error
+          reject({
+            reason: 'Unable to download page'
+          });
+        }
       });
-    }
+    });
+  },
+
+  // function to show the result in the resut view
+  showResult(objective){
+    // resgistered uri of the result view
+    uri = 'atom://test-package-result'
+    // open the editor
+    atom.workspace.open(uri, split= 'right', searchAllPanes= true).then( testPackageView => {
+        if (testPackageView instanceof TestPackageView)
+        {
+          // call to method of result view class
+          testPackageView.renderAnswer(objective);
+          // to activate the previous editor
+          atom.workspace.activatePreviousPane();
+        }
+      });
+  }
 
 };
